31346906cf0c49104ea4f2ec0367f90730194340
tscpu172 <43422215+tscpu172@users.noreply.github.com>
2019-06-08 22:17:20 -0400

    Add files via upload

---
+++ Project/prj-ece650/build/CMakeFiles/prj-measure.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prj-measure.dir/prj-measure.cpp.o"
+  "prj-measure.pdb"
+  "prj-measure"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prj-measure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
